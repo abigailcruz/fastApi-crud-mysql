6052199b7b1060d7859bc6ca9755df97f8a6578e
abigailcruz <jhojairamarquez.99@gmail.com>
2023-06-11 20:01:45 -0600

    API REST CRUD de usuarios con FastAPI y conexión a

--- schema/__init__.py
+++ schema/__init__.py
@@ -10,13 +10,12 @@ user = APIRouter()
 
 @user.get("/")
 def root():
-    return {"message": " Hi, I am FastApi with router" }
+    return {"message": "Hola, soy FastApi con router" }
 
 @user.post("/api/user", status_code=HTTP_201_CREATED)
 def create_user(data_user: UserSchema):
         new_user = data_user.dict()
         new_user['user_pass'] = generate_password_hash(data_user.user_pass, "pbkdf2:sha256:30", 30)
         conn.execute(users.insert().values(new_user))
-        conn.commit()
         return Response(status_code=HTTP_201_CREATED)
 
